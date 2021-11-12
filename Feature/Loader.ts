be43de6d4eeda19b5dfc93027880bacea7cd1159
Jan Siwek <jan.siwek94@gmail.com>
2021-11-12 19:35:56 +0100

    Dev

--- Feature/Loader.ts
+++ Feature/Loader.ts
@@ -12,37 +12,92 @@ export interface IScenario {
 
 export interface IFeature {
     name: string;
+    description: string;
     backgroundSteps: IStep[];
     scenarios: IScenario[];
 }
 
-const gherkinSectionExpr = /^([A-Za-z ]+)\:\s{0,}(.*)(?:\r\n)?$/;
-const gherkinStepExpr = /^(?:given|when|then|and|but)(.*)$/i;
-const gherkinCommentExpr = /^\#(?:.*)$/;
+export async function loadFeature(featurePath: string) {
+    let scenarioId = 1;
 
-enum GherkinScope {
-    Feature = 0,
-    Background = 1,
-    Scenario = 2,
-    ScenarioOutline = 3,
-    Examples = 4
+    const exists = await FsAsync.exists(featurePath);
+
+    if (!exists)
+        throw new Error(`Feature not found: ${featurePath}`);
+
+    const fileContent = (await FsAsync.readFile(featurePath)).replace(/^(?:\s*|\s*#.*?)$/mgs, ""); // Clear comments and empty lines
+
+    const gherkinSectionExpr = /(?:(Feature|Background|Scenario Outline|Scenario)):([\S ]*)(.*?)(?=Background|Scenario Outline|Scenario|$)/gsi;
+    const gherkinSectionList = Array.from(fileContent.matchAll(gherkinSectionExpr));
+
+    if (gherkinSectionList.length === 0)
+        throw new Error("Could not parse feature file.");
+
+    type TSectionType = "feature" | "scenario" | "background" | "scenario outline";
+
+    const feature: IFeature = {
+        name: null,
+        description: null,
+        backgroundSteps: [],
+        scenarios: []
+    };
+
+    for (const match of gherkinSectionList) {
+        const sectionType: TSectionType = match[1].trim().toLowerCase() as TSectionType;
+        const sectionName = match[2]?.trim();
+        const sectionContent = match[3]?.trim();
+
+        switch (sectionType) {
+            case "feature":
+                feature.name = sectionName;
+                feature.description = sectionContent;
+                break;
+            case "scenario":
+                const scenario: IScenario = {
+                    id: ++scenarioId,
+                    name: sectionName,
+                    steps: loadSteps(sectionContent.split("\n"), StepType.Scenario)
+                }
+
+                const lastScenario = feature.scenarios.last();
+                if (lastScenario)
+                    lastScenario.nextScenarioId = scenario.id;
+
+                feature.scenarios.push(scenario);
+                break;
+            case "background":
+                feature.backgroundSteps = loadSteps(sectionContent.split("\n"), StepType.Background)
+                break;
+            case "scenario outline":
+                break;
+            default:
+                throw new Error("Unknown section: " + sectionType);
+        }
+    }
+
+    return feature;
 }
 
-function getGherkinScope(text: string) {
-    switch (text.toLowerCase()) {
-        case "feature":
-            return GherkinScope.Feature;
-        case "background":
-            return GherkinScope.Background;
-        case "scenario":
-            return GherkinScope.Scenario;
-        default:
-            throw new Error("Scope not found: " + text);
+let stepId = 1;
+
+function loadSteps(lines: string[], type: StepType) {
+    const steps: IStep[] = [];
+
+    for (const line of lines) {
+        const step = parseStep(++stepId, type, line);
+
+        const lastStep = steps.last();
+        if (lastStep)
+            lastStep.nextStepId = step.id;
+
+        steps.push(step);
     }
+
+    return steps;
 }
 
 function parseStep(stepId: number, type: StepType, line: string) {
-    const stepMatch = gherkinStepExpr.exec(line);
+    const stepMatch = /^(?:given|when|then|and|but)(.*)$/i.exec(line.trim());
     if (!stepMatch)
         throw new Error("Incorrect step format: " + line);
 
@@ -52,94 +107,4 @@ function parseStep(stepId: number, type: StepType, line: string) {
     const step: IStep = { id: stepId, type, name: line, definition: stepDef };
 
     return step;
-}
-
-export async function loadFeature(featurePath: string) {
-    let scenarioId = 1;
-    let stepId = 1;
-
-    const exists = await FsAsync.exists(featurePath);
-
-    if (!exists)
-        throw new Error(`Feature not found: ${featurePath}`);
-
-    const featureContent = await FsAsync.readFile(featurePath);
-    const featureLines = featureContent.split("\n");
-
-    const features: IFeature[] = [];
-
-    let currentScope: GherkinScope;
-
-    for (let i = 0; i < featureLines.length; i++) {
-        const line = featureLines[i].trim();
-
-        if (line === "")
-            continue;
-
-        if (gherkinCommentExpr.exec(line))
-            continue; // skip comments
-
-        const sectionMatch = gherkinSectionExpr.exec(line);
-        if (sectionMatch) {
-            const scope = getGherkinScope(sectionMatch[1]);
-
-            switch (scope) {
-                case GherkinScope.Feature:
-                    features.push({ name: sectionMatch[2], scenarios: [], backgroundSteps: [] });
-                    currentScope = scope;
-                    break;
-                case GherkinScope.Scenario:
-                    const lastFeature = features.last();
-                    const scenario = { id: ++scenarioId, name: sectionMatch[2], steps: [] };
-
-                    const lastScenario = lastFeature.scenarios.last();
-                    if (lastScenario)
-                        lastScenario.nextScenarioId = scenario.id;
-
-                    lastFeature.scenarios.push(scenario);
-                    currentScope = scope;
-                    break;
-                case GherkinScope.Background:
-                    currentScope = scope;
-                    break;
-                default:
-                    throw new Error(`Unexpected section: ${sectionMatch[1]}.`);
-            }
-        } else {
-            switch (currentScope) {
-                case GherkinScope.Feature:
-                    throw new Error("Unexpected line in feature scope.");
-
-                case GherkinScope.Background:
-                    const backgroundStep = parseStep(++stepId, StepType.Background, line);
-                    const lastFeature = features.last();
-
-                    const lastBackgroundStep = lastFeature.backgroundSteps.last();
-                    if (lastBackgroundStep)
-                        lastBackgroundStep.nextStepId = backgroundStep.id;
-
-                    lastFeature.backgroundSteps.push(backgroundStep);
-
-                    break;
-                case GherkinScope.Scenario:
-                    const step = parseStep(++stepId, StepType.Scenario, line);
-                    const lastScenario = features.last().scenarios.last();
-
-                    const lastStep = lastScenario.steps.last();
-                    if (lastStep)
-                        lastStep.nextStepId = step.id;
-
-                    lastScenario.steps.push(step);
-                    break;
-                default:
-                    throw new Error("Unexpected scope: " + currentScope);
-            }
-        }
-    }
-
-    // Should only support single feature per file, so throw error if found more
-    if (features.length > 1)
-        throw new Error("Multiple features per file are not allowed.");
-
-    return features[0];
 }
