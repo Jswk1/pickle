9f4d97fe6397fa613117ba7084ad1d1090aa053e
Jan Siwek <jan.siwek@sofynetech.com>
2022-02-21 10:58:00 +0100

    Stacktrace length increase

--- bin/pickle.js
+++ bin/pickle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+Error.stackTraceLimit = 20;
 if (process.argv.some(e => e === "-d"))
     require("ts-node").register();
 require("../Runner").default();
--- bin/pickle.ts
+++ bin/pickle.ts
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+
+Error.stackTraceLimit = 20;
+
 if (process.argv.some(e => e === "-d"))
     require("ts-node").register();
 
